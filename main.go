23b102271029086474a1f0d11da5f62c9b7445a6
Zachary Acreman <zakk.acreman@scriptrock.com>
2016-03-31 10:14:19 -0700

    Changed panic() to log.Fatal()

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"github.com/ScriptRock/go-appstatus/status_api"
 	"net/http"
+	"log"
 )
 
 func main() {
-	panic(http.ListenAndServe(":8080", status_api.Handlers()))
+	log.Fatal(http.ListenAndServe(":8080", status_api.Handlers()))
 }
