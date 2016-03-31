45c51ce2cdcb874b7d7b2ff4e62343a34ce4f9b1
Zachary Acreman <zakk.acreman@scriptrock.com>
2016-03-30 21:20:33 -0700

    Factored some setup out of the tests

--- status_api/api_test.go
+++ status_api/api_test.go
@@ -6,20 +6,38 @@ import (
 	"net/http/httptest"
 )
 
-func TestHomeRedirects(t *testing.T) {	
+func beforeTest(method string, url string) (*httptest.ResponseRecorder, *http.Request, error) {
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/index.html", nil)
+	req, err := http.NewRequest(method, url, nil)
 
-	handler, pattern := Handlers().Handler(req)
-	handler.ServeHTTP(w, req)
+	return w, req, err
+}
 
-	// Root returns a redirect for directory indexes
-	if w.Code != http.StatusMovedPermanently {
-		t.Errorf("Home page didn't return %v, returned %v", http.StatusMovedPermanently, w.Code)
-	}
+
+// I can't figure out how to do content testing;
+// although the server works, when I test it I only get
+// 301s and 404s. Have to try to test this indirectly.
+// Should ask somebody.
+func TestHomeUsesCorrectPattern(t *testing.T) {
+	_, req, _ := beforeTest("GET", "/index.html")
+
+	_, pattern := Handlers().Handler(req)
 
 	// Handler pattern should be "/"
 	if pattern != "/" {
 		t.Errorf("Pattern handler should be \"\\\", was %s", pattern)
 	}
+}
+
+func TestHomeRedirects(t *testing.T) {	
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/index.html", nil)
+
+	handler, _ := Handlers().Handler(req)
+	handler.ServeHTTP(w, req)
+
+	// Root returns a redirect for directory indexes
+	if w.Code != http.StatusMovedPermanently {
+		t.Errorf("Home page didn't return %v, returned %v", http.StatusMovedPermanently, w.Code)
+	}	
 }
