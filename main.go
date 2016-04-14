5022074d7986c2174b6b70b21cb9690666857544
Chad Hedstrom <chad.hedstrom@scriptrock.com>
2016-04-14 14:51:07 -0700

    syslog logging for containers

--- main.go
+++ main.go
@@ -4,8 +4,21 @@ import (
 	"github.com/hadlock/go-rebound/status_api"
 	"net/http"
 	"log"
+	"bufio"
 )
 
 func main() {
+	// redirect stdout into syslog
+	go func() {
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			line := scanner.Text()
+			log.Debug("%s", line)
+		}
+	}()
+	
+	log.Debug("Num of CPU's: %d", cpus)
+	
+	// le web server
 	log.Fatal(http.ListenAndServe(":8080", status_api.Handlers()))
 }
