f8b3fe1cf502129f73d13a8ce55518ddfc1955db
Chad Hedstrom <chad.hedstrom@scriptrock.com>
2016-04-14 15:50:33 -0700

    container logging

--- main.go
+++ main.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	"github.com/hadlock/go-rebound/status_api"
+	"github.com/hadlock/go-rebound/log"
 	"net/http"
 	"log"
-	"bufio"
+	"bufio"			//for logs to container
+	"os"			//for logs to container
+	
+	
+	
+	
 )
 
 func main() {
 	// redirect stdout into syslog
+	oldStdout := os.Stdout
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout = w
+	
 	go func() {
 		scanner := bufio.NewScanner(r)
 		for scanner.Scan() {
 			line := scanner.Text()
-			log.Debug("%s", line)
+			log.Info("%s", line)
 		}
 	}()
 	
-	log.Debug("Num of CPU's: %d", cpus)
+	log.Info("Hello World")
 	
 	// le web server
 	log.Fatal(http.ListenAndServe(":8080", status_api.Handlers()))
