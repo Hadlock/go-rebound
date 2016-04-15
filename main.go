67ed51c7398ea5c08ed88855ee6e27a86a031537
Chad Hedstrom <chad.hedstrom@scriptrock.com>
2016-04-14 18:08:31 -0700

    slash n

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 
 
 func main() {
-	log.Println("Hello World")
+	log.SetFlags(0)
+	log.Println("go-rebound v0.0.1 Listening on port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", status_api.Handlers()))
 	
 }
