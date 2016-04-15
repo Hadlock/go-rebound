c2f874ec558c0aa7cd648410c5468bce27ff6b09
Chad Hedstrom <chad.hedstrom@scriptrock.com>
2016-04-14 17:29:21 -0700

    i can log gud

--- main.go
+++ main.go
@@ -2,35 +2,13 @@ package main
 
 import (
 	"github.com/hadlock/go-rebound/status_api"
-	"github.com/hadlock/go-rebound/log"
 	"net/http"
-	"bufio"			//for logs to container
-	"os"			//for logs to container
-	
-	
-	
-	
-)
+    "log"
+	)
+
 
 func main() {
-	// redirect stdout into syslog
-	//oldStdout := os.Stdout
-	r, w, err := os.Pipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	os.Stdout = w
-	
-	go func() {
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			line := scanner.Text()
-			log.Info("%s", line)
-		}
-	}()
-	
-	log.Warning("Hello World")
-	
-	// le web server
+	log.Println("Hello World")
 	log.Fatal(http.ListenAndServe(":8080", status_api.Handlers()))
+	
 }
